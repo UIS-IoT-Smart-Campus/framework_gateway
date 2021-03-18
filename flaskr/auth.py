510c90bd573a64b2d82318f78b23a528bd72b3fe
han253 <han315730@gmail.com>
2021-03-18 15:01:21 +0000

    add auth.py views

---
+++ flaskr/auth.py
@@ -0,0 +1,106 @@
+import functools
+
+from flask import(
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flask.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+#Metodos para conección a base de datos y autenticación
+
+
+#Metodo para saber desde otras vistas si un usuario está autenticado
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+#Metodo para que se retorne la sección de usuario en cada request. 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
+
+#Rutas para vistas
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    """ Metodo para registrar usuarios en la app gateway """
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        """ Metodo para loguin de usuarios en la app web gateway """
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@bp.route('/logout')
+def logout():
+    """Metodo para logout de usuario"""
+    session.clear()
+    return redirect(url_for('index'))
