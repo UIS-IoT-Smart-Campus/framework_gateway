35d2fabbfb46c2b0afa9602b8ef5cc894de59183
han253 <han315730@gmail.com>
2022-05-02 22:27:50 -0500

    add graph 3d.js visualization to dashboard

--- exp.sh
+++ exp.sh
@@ -1,3 +1,4 @@
-export FLASK_APP=flaskr
+export FLASK_APP=app
 export FLASK_ENV=development
 export FLASK_DEBUG=0
+export BROKERURL=192.168.0.125
--- flaskr/dashboard.py
+++ flaskr/dashboard.py
@@ -4,6 +4,9 @@ from flask import (
 
 from auth import login_required
 
+from app import db
+from models import Device
+
 
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
@@ -14,4 +17,19 @@ bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 @login_required
 def index():
     """ Dashboard Index Section"""
-    return render_template('dashboard/index.html')
+    nodes = []
+    links = []
+    devices_count = 0
+
+    #Add self-node
+    node = {"name":"Gateway","type":"Gateway"}
+    nodes.append(node)
+
+    devices = db.session.query(Device).all()
+    for device in devices:
+        node = {"name":device.name,"type":"Device"}
+        nodes.append(node)
+        devices_count+=1
+        links.append({"source":0,"target":devices_count})        
+
+    return render_template('dashboard/index.html', nodes = nodes, links = links)
